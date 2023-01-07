9009fa263900c6ab51f1e01b2d820b2d956850f1
Aditya pawar <aditya.p@prausam.com>
2023-01-08 01:15:55 +0530

    First Commit. Empty project with just title.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.mainDiv}>
+      <h1 className="text-3xl font-bold p-0 m-0">Four Tasks</h1>
+    </div>
+  );
+};
+
+export default App;
